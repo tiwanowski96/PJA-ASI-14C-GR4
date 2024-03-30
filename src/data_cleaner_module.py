42f4d150e6bd43114cb6f0903cb9056d217afda7
unknown <s23087@pjwstk.edu.pl>
2024-03-30 16:11:14 +0100

    Added data clening function, data analysis and enviroment with matplot

--- src/data_cleaner_module.py
+++ src/data_cleaner_module.py
@@ -1,5 +1,11 @@
-import import_data_module
+import pandas as pd
 
-data = import_data_module.import_dataframe_from_csv("model_data/CrabAgePrediction.csv")
+def clean_Data(dataframe) -> pd.DataFrame:
 
-print(data.head())
+    dataframe = dataframe[dataframe['Viscera Weight'] < 17.5]
+    dataframe = dataframe[dataframe['Shell Weight'] <= 24]
+    dataframe = dataframe[dataframe['Shucked Weight'] <= 36]
+    dataframe = dataframe[dataframe['Weight'] <= 78]
+    dataframe = dataframe[dataframe['Height'] < 1]
+
+    return dataframe
