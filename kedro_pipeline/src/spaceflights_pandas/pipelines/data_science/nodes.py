10dcabd62c63a5a2ed398832b8d940a2599de8dd
Tomasz Iwanowski <tomasz.iwanowski@ericsson.com>
2024-04-17 18:08:28 +0200

    Add runtime & default parameters

--- kedro_pipeline/src/spaceflights_pandas/pipelines/data_science/nodes.py
+++ kedro_pipeline/src/spaceflights_pandas/pipelines/data_science/nodes.py
@@ -10,9 +10,17 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
+logger = logging.getLogger(__name__)
+
 # Input: dataframe, name of target column
 # Output: numpy arrays containing splitted data as X_train, X_test, y_train, y_test
 def split_data(prepared_dataframe: pd.DataFrame,parameters: Dict) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+
+    logger.info(
+        f'Used parameters:\n \
+        test_size: {parameters["test_size"]}\n \
+        random_state: {parameters["random_state"]}'
+    )
 
     feature = prepared_dataframe.drop(columns=['Age'])
     target = prepared_dataframe['Age']
@@ -25,11 +33,24 @@ def split_data(prepared_dataframe: pd.DataFrame,parameters: Dict) -> Tuple[np.nd
 # Input: 2 arrays one containing features and another targets, model max_depth, max_features, n_estimators, min_samples_split, min_samples_leaf
 # Notice! Defaults are that of the model provided by sklearn
 # Output: Random forest model
-def create_rf_model(X_train: np.ndarray, y_train: np.ndarray, 
-                    in_max_depth=None, in_max_features=1.0, in_n_estimators=100, in_min_samples_split=2, in_min_samples_leaf=1) -> RandomForestRegressor:
+def create_rf_model(X_train: np.ndarray, y_train: np.ndarray, parameters: Dict) -> RandomForestRegressor:
 
-    rf_model = RandomForestRegressor(max_depth=in_max_depth, max_features=in_max_features, n_estimators=in_n_estimators, 
-                                     min_samples_leaf=in_min_samples_leaf, min_samples_split=in_min_samples_split)
+    logger.info(
+        f'Used parameters:\n \
+        max_depth: {parameters["max_depth"]}\n \
+        max_features: {parameters["max_features"]}\n \
+        n_estimators: {parameters["n_estimators"]}\n \
+        min_samples_leaf: {parameters["min_samples_leaf"]}\n \
+        min_samples_split: {parameters["min_samples_split"]}\n' 
+    )
+
+    rf_model = RandomForestRegressor(
+        max_depth=parameters["max_depth"],
+        max_features=parameters["max_features"],
+        n_estimators=parameters["n_estimators"], 
+        min_samples_leaf=parameters["min_samples_leaf"],
+        min_samples_split=parameters["min_samples_split"]
+    )
     rf_model.fit(X_train, y_train)
 
     return rf_model
