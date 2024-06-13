c19b9ecb30c960321c3ae2ddbac9ff6a02cdbc20
Patryk Polnik <s22515@pjwstk.edu.pl>
2024-06-13 14:21:14 +0200

    pipeline registry updated

--- kedro_pipeline/src/crab_prediction/pipeline_registry.py
+++ kedro_pipeline/src/crab_prediction/pipeline_registry.py
@@ -7,7 +7,8 @@ from kedro.pipeline import Pipeline
 import crab_prediction.pipelines.data_science as ds
 import crab_prediction.pipelines.data_processing as dp
 import crab_prediction.pipelines.autogluon_pipeline as autogluonPip
-import crab_prediction.pipelines.merged_pipeline as merged
+import crab_prediction.pipelines.deployment_pipeline as deployment
+import crab_prediction.pipelines.challenger_pipeline as challenger
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -19,12 +20,13 @@ def register_pipelines() -> Dict[str, Pipeline]:
     data_processing_pipeline = dp.create_pipeline()
     data_science_pipeline = ds.create_pipeline()
     autogluon_pipeline = autogluonPip.create_pipeline()
-    merged_pipeline = merged.create_pipeline()
+    deployment_pipeline = deployment.create_pipeline()
+    challenger_pipeline = challenger.create_pipeline()
 
     # pipelines = find_pipelines()
     # pipelines["__default__"] = sum(pipelines.values())
     return {
-        "__default__": data_processing_pipeline + merged_pipeline,
+        "__default__": data_processing_pipeline + challenger_pipeline + deployment_pipeline,
         "auto_ml": data_processing_pipeline + autogluon_pipeline,
         "raw": data_processing_pipeline + data_science_pipeline
     }
