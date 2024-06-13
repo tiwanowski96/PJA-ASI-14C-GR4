5382e8430db00672f707d497166fd74f957e2b7d
Patryk Polnik <s22515@pjwstk.edu.pl>
2024-06-13 14:19:31 +0200

    merged pipeline splited in challenger_pipeline and deployment_pipeline

---
+++ kedro_pipeline/src/crab_prediction/pipelines/challenger_pipeline/nodes.py
@@ -0,0 +1,98 @@
+from autogluon.tabular import TabularDataset, TabularPredictor
+from typing import Dict, Any, Tuple, Union
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import pandas as pd
+import numpy as np
+import logging
+import os
+import math
+import wandb
+import pickle
+
+logger = logging.getLogger(__name__)
+
+
+def split_data_for_autogluon(preprocessed_data: pd.DataFrame, parameters: Dict) -> Tuple[np.ndarray, np.ndarray]:
+    logger.info(
+        f'Split used parameters:\n \
+        test_size: {parameters["test_size"]}\n \
+        random_state: {parameters["random_state"]}'
+    )
+
+    train = preprocessed_data.sample(frac=(1-parameters["test_size"]), random_state=parameters["random_state"])
+    test = preprocessed_data.drop(train.index)
+
+    return train, test
+
+def create_autogluon_model(train_set: pd.DataFrame, parameters: Dict) -> TabularPredictor:
+
+    logger.info(
+        f'Autogluon used parameters:\n \
+        target_column: {parameters["target_column"]}\n \
+        path: {parameters["path"]}\n \
+        problem_type: {parameters["problem_type"]}\n \
+        eval_metric: {parameters["eval_metric"]}\n \
+        presets: {parameters["presets"]}'
+    )
+
+    train_data = TabularDataset(train_set)
+
+    predictor = TabularPredictor(label=parameters["target_column"],path=parameters["path"],problem_type=parameters["problem_type"], eval_metric=parameters["eval_metric"]).fit(train_data, presets=parameters["presets"])
+
+    return predictor
+
+
+def split_data_for_rf(prepared_dataframe: pd.DataFrame,parameters: Dict) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+
+    logger.info(
+        f'Used parameters:\n \
+        test_size: {parameters["test_size"]}\n \
+        random_state: {parameters["random_state"]}'
+    )
+
+    feature = prepared_dataframe.drop(columns=['Age'])
+    target = prepared_dataframe['Age']
+
+    X_train, X_test, y_train, y_test =  train_test_split(feature, target, test_size=parameters["test_size"], random_state=parameters["random_state"])
+
+    return X_train, X_test, y_train, y_test
+
+
+# Input: 2 arrays one containing features and another targets, model max_depth, max_features, n_estimators, min_samples_split, min_samples_leaf
+# Notice! Defaults are that of the model provided by sklearn
+# Output: Random forest model
+def create_rf_model(X_train: np.ndarray, y_train: np.ndarray, parameters: Dict) -> RandomForestRegressor:
+
+    logger.info(
+        f'Used parameters:\n \
+        max_depth: {parameters["max_depth"]}\n \
+        max_features: {parameters["max_features"]}\n \
+        n_estimators: {parameters["n_estimators"]}\n \
+        min_samples_leaf: {parameters["min_samples_leaf"]}\n \
+        min_samples_split: {parameters["min_samples_split"]}\n' 
+    )
+
+    rf_model = RandomForestRegressor(
+        max_depth=parameters["max_depth"],
+        max_features=parameters["max_features"],
+        n_estimators=parameters["n_estimators"], 
+        min_samples_leaf=parameters["min_samples_leaf"],
+        min_samples_split=parameters["min_samples_split"]
+    )
+    rf_model.fit(X_train, y_train)
+
+    return rf_model
+    
+def split_data(prepared_dataframe: pd.DataFrame,parameters: Dict) -> list:
+    train, test = split_data_for_autogluon(prepared_dataframe, parameters)
+    X_train, X_test, y_train, y_test = split_data_for_rf(prepared_dataframe, parameters)
+    datasets_list = [train, test, X_train, X_test, y_train, y_test]
+    return datasets_list
+
+def create_model(datasets_list: list, parameters: Dict) -> TabularPredictor | RandomForestRegressor:
+    if parameters["model_creation"]:
+        return create_autogluon_model(datasets_list[0], parameters)
+    else:
+        return create_rf_model(datasets_list[2], datasets_list[4], parameters)
---
+++ kedro_pipeline/src/crab_prediction/pipelines/deployment_pipeline/nodes.py
@@ -0,0 +1,109 @@
+from autogluon.tabular import TabularDataset, TabularPredictor
+from typing import Dict, Any, Tuple, Union
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import pandas as pd
+import numpy as np
+import logging
+import os
+import math
+import wandb
+import pickle
+
+logger = logging.getLogger(__name__)
+
+   
+# def compare_models(my_bool:bool,automl_challenger: dict,random_challenger: dict) -> None:
+#     champion_accuracy = 0.90#champion['accuracy']
+#     if my_bool==True:
+#         challenger_accuracy = automl_challenger['accuracy']
+#     else:
+#         challenger_accuracy = random_challenger['accuracy']
+
+#     if challenger_accuracy > champion_accuracy:
+#         logger.info("Challenger model is better than the champion model.")
+#     else:
+#         logger.info("Challenger model is not better than the champion model.")
+#     pass
+
+def load_champion_model() -> Union[TabularPredictor, RandomForestRegressor, str]:
+    models_path = "data//06_models//champion"
+    champion_model_path = os.path.join(models_path, "champion.pickle")
+    if os.path.isfile(champion_model_path):
+        with open(champion_model_path, 'rb') as file:
+            champion_model = pickle.load(file)
+        return champion_model
+    else:
+        return ""
+    
+
+
+def compare_models(challenger_model:TabularPredictor | RandomForestRegressor, champion_model:TabularPredictor | RandomForestRegressor | str, datasets_list: list, parameters:Dict) -> TabularPredictor | RandomForestRegressor:
+  
+
+    if isinstance(champion_model, RandomForestRegressor) and parameters["model_creation"]:
+
+        rf_champion_pred = champion_model.predict(datasets_list[3])
+        r2_rf_champion = r2_score(datasets_list[5], rf_champion_pred)
+
+        challenger_model_pred = challenger_model.evaluate(datasets_list[1])
+        r2_tabular_challenger = challenger_model_pred.get('r2')
+
+        if r2_rf_champion>r2_tabular_challenger:
+            logger.info("Champion model is better than the challenger model.")
+            return champion_model
+        else:
+            logger.info("Challenger model is better than the champion model.")
+            return challenger_model
+
+    elif isinstance(champion_model, RandomForestRegressor) and not parameters["model_creation"]:
+
+        
+        rf_champion_pred = champion_model.predict(datasets_list[3])
+        r2_rf_champion = r2_score(datasets_list[5], rf_champion_pred)
+
+        rf_challenger_pred = challenger_model.predict(datasets_list[3])
+        r2_rf_challenger = r2_score(datasets_list[5], rf_challenger_pred)
+        logger.info(f"r2_rf_challenger: {r2_rf_challenger}, r2_rf_champion: {r2_rf_champion}")
+
+        if r2_rf_champion > r2_rf_challenger:
+            logger.info("Champion model is better than the challenger model.")
+            return champion_model
+        else:
+            logger.info("Challenger model is better than the champion model.")
+            return challenger_model
+
+    elif isinstance(champion_model, TabularPredictor) and parameters["model_creation"]:
+
+        tabular_champion_pred = champion_model.evaluate(datasets_list[1])
+        r2_tabular_champion = tabular_champion_pred.get('r2')
+
+
+        challenger_model_pred = challenger_model.evaluate(datasets_list[1])
+        r2_tabular_challenger = challenger_model_pred.get('r2')
+
+        if r2_tabular_champion > r2_tabular_challenger:
+            logger.info("Champion model is better than the challenger model.")
+            return champion_model
+        else:
+            logger.info("Challenger model is better than the champion model.")
+            return challenger_model
+
+    elif isinstance(champion_model, TabularPredictor) and not parameters["model_creation"]:
+        tabular_champion_pred = champion_model.evaluate(datasets_list[1])
+        r2_tabular_champion = tabular_champion_pred.get('r2')
+
+        rf_challenger_pred = challenger_model.predict(datasets_list[3])
+        r2_rf_challenger = r2_score(datasets_list[5], rf_challenger_pred)
+
+        logger.info(f"r2_rf_challenger: {r2_rf_challenger}, r2_rf_champion: {r2_tabular_champion}")
+        if r2_tabular_champion > r2_rf_challenger:
+            logger.info("Champion model is better than the challenger model.")
+            return champion_model
+        else:
+            logger.info("Challenger model is better than the champion model.")
+            return challenger_model
+    else:
+        logger.info("Champion model does not exists, challenger model is new champion.")
+        return challenger_model
