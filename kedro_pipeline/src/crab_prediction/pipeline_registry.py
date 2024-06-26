71c00ba175b221775e8dc13340eacd788e09d22b
unknown <s23087@pjwstk.edu.pl>
2024-06-26 20:00:42 +0200

    Updated chart and rename file

--- kedro_pipeline/src/crab_prediction/pipeline_registry.py
+++ kedro_pipeline/src/crab_prediction/pipeline_registry.py
@@ -6,7 +6,7 @@ from kedro.pipeline import Pipeline
 
 import crab_prediction.pipelines.data_science as ds
 import crab_prediction.pipelines.data_processing as dp
-import crab_prediction.pipelines.autogluon_pipeline as autogluonPip
+import crab_prediction.pipelines.auto_ml__pipeline as autoMlPip
 import crab_prediction.pipelines.deployment_pipeline as deployment
 import crab_prediction.pipelines.challenger_pipeline as challenger
 
@@ -19,7 +19,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     """
     data_processing_pipeline = dp.create_pipeline()
     data_science_pipeline = ds.create_pipeline()
-    autogluon_pipeline = autogluonPip.create_pipeline()
+    auto_ml_pipeline = autoMlPip.create_pipeline()
     deployment_pipeline = deployment.create_pipeline()
     challenger_pipeline = challenger.create_pipeline()
 
@@ -27,6 +27,6 @@ def register_pipelines() -> Dict[str, Pipeline]:
     # pipelines["__default__"] = sum(pipelines.values())
     return {
         "__default__": data_processing_pipeline + challenger_pipeline + deployment_pipeline,
-        "auto_ml": data_processing_pipeline + autogluon_pipeline,
+        "auto_ml": data_processing_pipeline + auto_ml_pipeline,
         "raw": data_processing_pipeline + data_science_pipeline
     }
